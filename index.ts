fae286076ba5e2baf85e090b90d0e7e674b29b28
yuchiki <yuuki_imai@gmail.com>
2020-06-22 23:44:28 +0900

    html

--- index.ts
+++ index.ts
@@ -9,15 +9,7 @@ async function main() {
   const TokyoCoronaDatas = await axios.get<TokyoCoronaData>(url);
 
 
-  const template = `
-<!DOCTYPE HTML>
-<html lang="ja">
-<head>
-  <title>fuga</title>
-</head>
-<body>piyo</body>
-</html>
-`;
+
 
   const app: express.Express = express();
 
@@ -38,7 +30,27 @@ async function main() {
   // Getのルーティング
   const router: express.Router = express.Router();
   router.get("/", (req: express.Request, res: express.Response) => {
-    res.send(TokyoCoronaDatas.data.data.slice(-5).map(datum => `${datum.diagnosed_date.slice(-2)}日: ${datum.count}人`).join(", "));
+    const length = req.params.dates ? Number(req.params.dates) : 5; // datesがクエリパラメータから取得できない
+    const lines = TokyoCoronaDatas.data.data.slice(-length).map(datum => `${datum.diagnosed_date.slice(-2)}日: ${datum.count}人`);
+
+    const template = `
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+  <meta property="og:title" content="東京の感染者数"/>
+  <meta property="og:type" content="website"/>
+  <meta property="og:description" content="${lines.join(", ")}"
+  <meta property="og:url" content="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/daily_positive_detail.json"/>
+  <title>fuga</title>
+</head>
+<body>
+    ${lines.join("<br>")}
+</body>
+</html>
+`;
+
+    res.send(template);
+
   });
 
   app.use(router);
